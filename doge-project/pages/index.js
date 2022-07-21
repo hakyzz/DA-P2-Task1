15b91b5e8791bd843d3cd2951cdd7db48b31075a
Hakan Arioglu <hakan.arioglu@namics.com>
2022-07-21 13:06:51 +0300

    setting up api routes in next js

--- doge-project/pages/index.js
+++ doge-project/pages/index.js
@@ -5,9 +5,11 @@ import useSWR from 'swr'
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Home() {
-  const { data, error } = useSWR("https://api-mainnet.magiceden.dev/v2/tokens/BK6xK21tXxrKeA85yvXQmMMVkkZSf4urNz7ZG9hzJqX", fetcher);
+  const { data: nftData, error: nftDataError } = useSWR("https://api-mainnet.magiceden.dev/v2/tokens/BK6xK21tXxrKeA85yvXQmMMVkkZSf4urNz7ZG9hzJqX", fetcher);
+  const { data: profileData, error: profileDataError } = useSWR("/api/profile", fetcher);
 
-  return  data && (
+
+  return nftData &&  profileData && (
       <div className="bg-gray-100 antialiased min-h-screen font-mono">
       <Head>
         <title>Hakyzz CV</title>
@@ -18,29 +20,25 @@ export default function Home() {
       <main className="max-w-5xl p-5 mx-auto">
         <div className="flex flex-col justify-center items-center mb-10">
           <div className="mb-5">
-            <Image className="rounded-xl" width={200} height={200} src={data.image} alt={data.name}/>
+            <Image className="rounded-xl" width={200} height={200} src={nftData.image} alt={nftData.name}/>
           </div>
           <div>
             <h1 className="text-3xl font-bold text-center">
-              Hakyzz the naked skelly
+              {profileData.name}
             </h1>
           </div>
         </div>
         <div className="grid md:grid-cols-2 gap-5">
           <div className="p-7 bg-white/50 rounded-xl">
             <h2 className="text-xl font-bold mb-5">About me</h2>
-            <p className="text-sm">Hi there, I'm Hakyzz. I love being challenged and engaged with projects that require work outside my comfort and knowledge set. Continuing to learn new languages and development techniques are important to me. Oh and I am happily married with my lovely wife since 2018.</p>
+            <p className="text-sm">{profileData.description}</p>
           </div>
           <div className="p-7 bg-white/50 rounded-xl">
             <h2 className="text-xl font-bold mb-5">Skills</h2>
             <div className="flex flex-wrap">
-              <span className="text-sm m-1 rounded-lg px-4 py-1.5 bg-[#a4d6b0]">JavaScript</span>
-              <span className="text-sm m-1 rounded-lg px-4 py-1.5 bg-[#a4d6b0]">React</span>
-              <span className="text-sm m-1 rounded-lg px-4 py-1.5 bg-[#a4d6b0]">Next</span>
-              <span className="text-sm m-1 rounded-lg px-4 py-1.5 bg-[#a4d6b0]">NodeJS</span>
-              <span className="text-sm m-1 rounded-lg px-4 py-1.5 bg-[#a4d6b0]">TypeScript</span>
-              <span className="text-sm m-1 rounded-lg px-4 py-1.5 bg-[#a4d6b0]">HTML/CSS</span>
-              <span className="text-sm m-1 rounded-lg px-4 py-1.5 bg-[#a4d6b0]">Sass</span>
+              {profileData.skills.map((skill, i) => (
+                <span key={i} className="text-sm m-1 rounded-lg px-4 py-1.5 bg-[#a4d6b0]">{skill}</span>
+              ))}
               </div>
           </div>
         </div>
