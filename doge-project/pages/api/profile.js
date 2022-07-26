81adcc38988efc24e3988c03e0ae2e9eb40b2501
Hakan Arioglu <hakan.arioglu@namics.com>
2022-07-26 08:22:09 +0300

    add mongodb as dependency

--- doge-project/pages/api/profile.js
+++ doge-project/pages/api/profile.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from "../../util/dbConfig";
+import { connectToDatabase } from "../../util/mongodb";
 
 export default async (req, res) => {
     const { db } = await connectToDatabase();
