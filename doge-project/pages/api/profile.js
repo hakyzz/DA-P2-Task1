52a13e6dfa8c96811410a2fc2c5c7bd1a4edc8f2
Hakan Arioglu <hakan.arioglu@namics.com>
2022-07-26 08:13:50 +0300

    fixing vercel build error

--- doge-project/pages/api/profile.js
+++ doge-project/pages/api/profile.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from "../../util/mongodb";
+import { connectToDatabase } from "../../util/mongodb.js";
 
 export default async (req, res) => {
     const { db } = await connectToDatabase();
